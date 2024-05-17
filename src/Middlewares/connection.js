d6fe81e3dd2de0ef49385023674072845b218c53
Anand Pandey <anand.pandey1052@gmail.com>
2024-05-17 16:04:32 +0530

    .

--- src/Middlewares/connection.js
+++ src/Middlewares/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   await mongoose.connect(
-    "mongodb+srv://anand0:anand0@amazon.nptltew.mongodb.net/?retryWrites=true&w=majority"
+   process.env.MONGODB_URI
   );
   console.log("Connected to database");
 };
