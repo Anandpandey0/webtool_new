bbba89626cd4a7eedb62015cf280d585f967208c
Anand Pandey <anand.pandey1052@gmail.com>
2024-05-20 14:45:43 +0530

    Create addBhoomiScoreDemo.js

---
+++ src/pages/api/addBhoomiScoreDemo.js
@@ -0,0 +1,36 @@
+
+import { connectSampleMongoDB } from "@/lib/mongodbSame";
+import { bhoomiscore } from "@/data/bhoomiscore";
+import BhoomiScore from "@/Models/bhoomiscore";
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      // Parse the request body
+      await connectSampleMongoDB();
+      const insertData = await BhoomiScore.insertMany(bhoomiscore);
+      res.status(200).json({success:true,data:insertData})
+ 
+    } catch (error) {
+      // Return error response
+      console.error(error);
+      res.json({ status: 500, message: "Internal server error" });
+    }
+  } else {
+    // Return method not allowed response for other HTTP methods
+    res.status(405).end();
+  }
+  if(req.method ==='GET'){
+    try {
+      // Parse the request body
+      await connectMongoDB();
+      const insertData = await Farmers.find();
+      res.status(200).json({success:true,data:insertData})
+  }
+  catch(error){
+    // Return error response
+    console.error(error);
+    res.json({ status: 500, message: "Internal server error" });
+  }
+}
+}
