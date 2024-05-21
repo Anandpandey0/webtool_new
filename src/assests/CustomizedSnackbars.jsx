cee34009ec01e9533e6d8613df23ce9349415b8a
Anand Pandey <anand.pandey1052@gmail.com>
2024-05-21 12:48:31 +0530

    Added toast when user sign in

---
+++ src/assests/CustomizedSnackbars.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+
+export default function CustomizedSnackbars({open}) {
+  const [openToast, setOpenToast] = React.useState(open);
+
+
+  // const handleClick = () => {
+  //   setOpen(true);
+  // };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenToast(false)
+
+
+    
+  };
+
+  return (
+    <div>
+      {/* <Button onClick={handleClick}>Open Snackbar</Button> */}
+      <Snackbar open={openToast} autoHideDuration={3000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity="success"
+          variant="filled"
+          sx={{ width: '100%' }}
+        >
+          Login Successfull
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PersistentDrawerLeft from '../assests/MiniDrawer'
 import MiniDrawer from '../assests/MiniDrawer'
+import { useSession } from 'next-auth/react'
+import CustomizedSnackbars from '@/assests/CustomizedSnackbars'
 
 const Dashboard = () => {
+  const {data:session} = useSession();
+  const [showToast ,setShowToast] = useState(false)
+  useEffect(()=>{
+    if(session)
+      setShowToast(true)
+    
+  },[session])
   return (
 
       <>
-     
+      {showToast && <CustomizedSnackbars open={showToast}/>}
       <MiniDrawer/>
       
     </>
